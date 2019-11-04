02da5cb9c6f21c4b143cf116a54909c526554921
yiyang1016 <yiyang908@gmail.com>
2019-11-05 00:22:38 +0800

    Pratical 2.3

--- app/src/main/java/com/example/practical_23/MainActivity.kt
+++ app/src/main/java/com/example/practical_23/MainActivity.kt
@@ -5,26 +5,37 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setListeners()
+        box_one_text.setBackgroundColor(Color.WHITE)
+        box_two_text.setBackgroundColor(Color.WHITE)
+        box_three_text.setBackgroundColor(Color.WHITE)
+        box_four_text.setBackgroundColor(Color.WHITE)
+        box_five_text.setBackgroundColor(Color.WHITE)
 
-    }
+        setListeners()
+        }
 
     private fun makeColored(view: View) {
         when (view.id) {
 
-            // Boxes using Color class colors for the background
             R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
-            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+            //R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+            R.id.box_two_text -> view.setBackgroundResource(R.drawable.ic_launcher_background)
             R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
             R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
             R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
+            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
+
             else -> view.setBackgroundColor(Color.LTGRAY)
+
         }
     }
 
@@ -37,14 +48,20 @@ class MainActivity : AppCompatActivity() {
         val boxFiveText = findViewById<TextView>(R.id.box_five_text)
 
         val rootConstraintLayout = findViewById<View>(R.id.constraint_layout)
+        val redButton = findViewById<TextView>(R.id.red_button)
+        val greenButton = findViewById<TextView>(R.id.green_button)
+        val yellowButton = findViewById<TextView>(R.id.yellow_button)
 
         val clickableViews: List<View> =
             listOf(
                 boxOneText, boxTwoText, boxThreeText,
-                boxFourText, boxFiveText, rootConstraintLayout
+                boxFourText, boxFiveText, rootConstraintLayout,
+                redButton, greenButton, yellowButton
             )
         for (item in clickableViews) {
             item.setOnClickListener { makeColored(it) }
         }
+
     }
+
 }
